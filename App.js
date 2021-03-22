e724a27346c189c7d9b7be85d9130552241fb823
Prince Tola <michelkogadou@gmail.com>
2021-03-22 16:11:10 +0000

    First commit

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Register from "./Register";
+import Colors from "./constants/Colors";
+import { Alert } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
 export default function App() {
+  const [showPassword, setShowPassword] = React.useState(false);
+  const [email, setEmail] = React.useState("");
+  const [first_name, setFirstName] = React.useState("");
+  const [last_name, setLastName] = React.useState("");
+  const [adresse, setAdresse] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [password_confirmation, setPassword_confirmation] = React.useState("");
+  const [errors, setErrors] = React.useState({});
+
+  const submit = async () => {
+    Alert.alert("Félicitation", "Connexion réussie");
+  };
+
+  console.log("password_confirmation", password_confirmation);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Register
+        OnSubmit={submit}
+        pressRedirectLogin={() => {
+          Alert.alert("redirect regidter");
+        }}
+        showPassword={showPassword}
+        setShowPassword={setShowPassword}
+        //first_name={first_name}
+        setFirstName={setFirstName}
+        //last_name={last_name}
+        setLastName={setLastName}
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        password_confirmation={password_confirmation}
+        setPassword_confirmation={setPassword_confirmation}
+        //adresse={adresse}
+        setAdresse={setAdresse}
+        errors={errors}
+        setErrors={setErrors}
+        textRedirectLogin="Je n'ai pas encore de compte"
+      />
     </View>
   );
 }
@@ -14,8 +53,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ Register.js
@@ -0,0 +1,279 @@
+import React from "react";
+import {
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
+  Alert,
+} from "react-native";
+import Yup from "./shared/validator";
+import Colors from "./constants/Colors";
+import { RenderButton, RenderInput } from "./shared/renderInput";
+import Icon from "react-native-vector-icons/Entypo";
+
+export default function Register({
+  setErrors,
+  errors,
+  setPassword,
+  setPassword_confirmation,
+  password,
+  password_confirmation,
+  setEmail,
+  setFirstName,
+  email,
+  first_name = false,
+  last_name = false,
+  phone = false,
+  adresse = false,
+  setPhone,
+  setLastName,
+  setAdresse,
+  setShowPassword,
+  showPassword = false,
+  title = "Register",
+  labelEmail = "Adresse mail",
+  labelFirstName = "Prénom",
+  labelLastName = "Nom",
+  labelAdresse = "Adresse",
+  labelPhone = "Téléphone",
+  labelPassword = "Mot de Passe",
+  labelPassword_confirmation = "Confirmer mot de passe",
+  titleStyle = {},
+  colorIconPassword = Colors.bgApp,
+  textConnexion = "S'inscrire",
+  connexionTitleStyle = {},
+  connexionButtonStyle = {},
+  leftIconPassword = {},
+  leftIconEmail = {},
+  leftIconFirstName = {},
+  leftIconLastName = {},
+  leftIconAdresse = {},
+  leftIconPhone = {},
+  textRedirectLogin,
+  textRedirectLoginStyle = {},
+  styles = {},
+  OnSubmit,
+  pressRedirectLogin,
+}) {
+  const Schema = Yup.object().shape({
+    email: Yup.string().email().required().label("Adresse Mail"),
+    first_name:
+      first_name !== false && Yup.string().min(1).required().label("Prénom"),
+    last_name:
+      last_name !== false && Yup.string().min(1).required().label("Nom"),
+    password: Yup.string().required().label("Mot de passe"),
+  });
+  const submit = () => {
+    if (password === password_confirmation) {
+      OnSubmit();
+    } else {
+      Alert.alert("Attention", "Vérifier mot de passe");
+    }
+  };
+  const [loading, setLoading] = React.useState(false);
+
+  let onChange = (text) => {
+    setEmail(text);
+  };
+
+  let onChangeFirstName = (text) => {
+    setFirstName(text);
+  };
+
+  let onChangeLastName = (text) => {
+    setLastName(text);
+  };
+
+  let onChangeAdresse = (text) => {
+    setAdresse(text);
+  };
+
+  let onChangePhone = (text) => {
+    setPhone(text);
+  };
+
+  let onChangePassword = (text) => {
+    setPassword(text);
+  };
+
+  let onChangePassword_confirmation = (text) => {
+    setPassword_confirmation(text);
+  };
+  return (
+    <View style={{ flex: 1, paddingHorizontal: 20, ...styles }}>
+      <ScrollView bounces={false}>
+        <View>
+          <Text
+            style={{
+              marginVertical: 30,
+              fontWeight: "bold",
+              color: "#454545",
+              fontSize: 36,
+              textAlign: "center",
+              ...titleStyle,
+            }}
+          >
+            {title}
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+          }}
+        >
+          {first_name !== false && (
+            <RenderInput
+              value={first_name}
+              errors={errors}
+              error={errors?.first_name}
+              label={labelFirstName}
+              onChange={onChangeFirstName}
+              leftIcon={leftIconFirstName}
+              autoCapitalize={"words"}
+            />
+          )}
+          {last_name !== false && (
+            <RenderInput
+              value={last_name}
+              errors={errors}
+              error={errors?.last_name}
+              label={labelLastName}
+              onChange={onChangeLastName}
+              leftIcon={leftIconLastName}
+              autoCapitalize={"words"}
+            />
+          )}
+
+          <RenderInput
+            value={email}
+            errors={errors}
+            error={errors?.email}
+            label={labelEmail}
+            onChange={onChange}
+            leftIcon={leftIconEmail}
+            textContentType={"username"}
+            autoCapitalize={"none"}
+            keyboardType={"email-address"}
+          />
+
+          {adresse !== false && (
+            <RenderInput
+              value={adresse}
+              errors={errors}
+              error={errors?.adresse}
+              label={labelAdresse}
+              onChange={onChangeAdresse}
+              leftIcon={leftIconAdresse}
+              autoCapitalize={"words"}
+            />
+          )}
+          {phone !== false && (
+            <RenderInput
+              value={phone}
+              errors={errors}
+              error={errors?.phone}
+              label={labelPhone}
+              onChange={onChangePhone}
+              leftIcon={leftIconPhone}
+              keyboardType={"number-pad"}
+            />
+          )}
+
+          <RenderInput
+            value={password}
+            errors={errors}
+            error={errors?.password}
+            label={labelPassword}
+            onChange={onChangePassword}
+            leftIcon={leftIconPassword}
+            textContentType={"password"}
+            rightIcon={
+              setShowPassword !== undefined &&
+              (!showPassword ? (
+                <TouchableOpacity
+                  onPress={() => setShowPassword(!showPassword)}
+                >
+                  <Icon name="eye" size={24} color={colorIconPassword} />
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity
+                  onPress={() => setShowPassword(!showPassword)}
+                >
+                  <Icon
+                    name="eye-with-line"
+                    size={24}
+                    color={colorIconPassword}
+                  />
+                </TouchableOpacity>
+              ))
+            }
+            secureTextEntry={!showPassword}
+          />
+
+          <RenderInput
+            value={password_confirmation}
+            errors={errors}
+            error={errors?.password_confirmation}
+            label={labelPassword_confirmation}
+            onChange={onChangePassword_confirmation}
+            leftIcon={leftIconPassword}
+            textContentType={"password"}
+            rightIcon={
+              setShowPassword !== undefined &&
+              (!showPassword ? (
+                <TouchableOpacity
+                  onPress={() => setShowPassword(!showPassword)}
+                >
+                  <Icon name="eye" size={24} color={colorIconPassword} />
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity
+                  onPress={() => setShowPassword(!showPassword)}
+                >
+                  <Icon
+                    name="eye-with-line"
+                    size={24}
+                    color={colorIconPassword}
+                  />
+                </TouchableOpacity>
+              ))
+            }
+            secureTextEntry={!showPassword}
+          />
+          <RenderButton
+            title={textConnexion}
+            Schema={Schema}
+            startLoad={() => setLoading(true)}
+            endLoad={() => setLoading(false)}
+            setErrors={(error) => setErrors(error)}
+            email={email}
+            password={password}
+            submit={submit}
+            styles={{ backgroundColor: "red" }}
+            titleStyle={connexionTitleStyle}
+            buttonStyle={connexionButtonStyle}
+          />
+        </View>
+        <TouchableOpacity
+          style={{
+            alignSelf: "flex-end",
+            marginTop: Dimensions.get("window").height * 0.05,
+          }}
+          onPress={pressRedirectLogin}
+        >
+          <Text
+            style={{
+              color: Colors.bgApp,
+              textAlign: "center",
+              fontSize: 14,
+              ...textRedirectLoginStyle,
+            }}
+          >
+            {textRedirectLogin}
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+}
